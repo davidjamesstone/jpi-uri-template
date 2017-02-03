eaf1e4699fb4825b7303c930a42d8d1c5d836dd2
David Stone <davidjamesstone@gmail.com>
2017-02-03 22:09:33 +0000

    Update

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
-// const parser = require('uri-templates')
 const Path = require('path')
 const pointer = require('json-pointer')
 const reduce = require('lodash.reduce')
-// const parser = require('uritemplate').parse
-const RefParser = require('json-schema-ref-parser')
-// const buildHref = require('json-schema-example-loader/lib/transformer').buildHref
-
-// const tpl = parser('/date/{colour}/{shape}')
-// const tpl1 = parser('/{year}/{month}/{day}{?orderBy,direction}')
-// const tpl2 = parser('{?query*}')
-// const tpl4 = parser('/prefix/{?params*}')
-// const customerSchemaPath = Path.resolve(__dirname, './schema/routes/customer.json')
 
 const throwError = function (e, msg) {
-  var err = new Error(msg + ': ' + e.message)
+  const err = new Error(msg + ': ' + e.message)
   err.stack = e.stack
   throw err
 }
@@ -21,36 +11,35 @@ const throwError = function (e, msg) {
 const parseHref = function (href, schema) {
   if (!href) return undefined
   // This will pull out all {/schema/pointers}
-  var pattern = /((?:{(?:#?(\/[\w\/]+))})+)+/g
-  var matches = href.match(pattern)
+  const pattern = /((?:{(?:#?(\/[\w\/]+))})+)+/g
+  const matches = href.match(pattern)
 
-  var rtn = {
-    matches: matches.map(match => {
+  const rtn = {
+    matches: !matches ? [] : matches.map(match => {
       return { match: match }
     })
   }
 
-  var queryIndex = href.indexOf('?')
-  var hasQuery = queryIndex > -1
+  const queryIndex = href.indexOf('?')
+  const hasQuery = queryIndex > -1
 
   try {
     rtn.href = reduce(rtn.matches, function (str, m, index) {
-      var match = m.match
+      const match = m.match
       // Remove the brackets so we can find the definition
-      var stripped = match.replace(/[{}]/g, '')
+      const stripped = match.replace(/[{}]/g, '')
       // Resolve the reference within the schema
-      var definition = pointer.get(schema, stripped.substring(1))
+      const definition = pointer.get(schema, stripped.substring(1))
       if (!definition) {
         throwError(new Error('Pointer reference'), 'Could not resolve href: ' + href)
       }
 
-      var basename = Path.basename(stripped)
+      const basename = Path.basename(stripped)
       rtn.matches[index].key = basename
       rtn.matches[index].type = hasQuery && href.indexOf(match) > queryIndex ? 'query' : 'param'
       rtn.matches[index].definition = definition
       // Replace the match with either example data or the last component of the pointer
-      var replacement = '{' + basename + '}'
-      // /my/{#/pointer} -> /my/example_value OR /my/:pointer
+      const replacement = '{' + basename + '}'
       return str.replace(match, replacement)
     }, href)
     rtn.path = hasQuery ? rtn.href.substr(0, rtn.href.indexOf('?')) : rtn.href
@@ -59,15 +48,5 @@ const parseHref = function (href, schema) {
     throwError(e, 'Could not build href: ' + href)
   }
 }
-
-// RefParser.dereference(customerSchemaPath, function (err, customerSchema) {
-//   if (err) {
-//     throw err
-//   }
-
-//   const path = parseHref('/customer/{#/definitions/identifier}/{./common/id.json}?{#/definitions/orderBy}', customerSchema)
-
-
-// })
 
 module.exports = parseHref
